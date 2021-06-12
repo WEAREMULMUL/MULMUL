ddde3710440ab4583b7ed7b44234682411c408ec
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-06-12 22:54:29 +0900

    [INIT] PROJECT - MULMUL

---
+++ src/main/java/com/excmul/ExcmulApplication.java
@@ -0,0 +1,13 @@
+package com.excmul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcmulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcmulApplication.class, args);
+    }
+
+}
