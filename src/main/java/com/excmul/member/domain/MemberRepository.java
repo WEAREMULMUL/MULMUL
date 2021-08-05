598aa182cd6625ad9aa9c54dec7121e2b4cb5fc0
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-08-05 16:53:35 +0900

    [BACKUP] 2021-05-05- : 16:53

--- src/main/java/com/excmul/member/domain/MemberRepository.java
+++ src/main/java/com/excmul/member/domain/MemberRepository.java
@@ -1,13 +1,18 @@
 package com.excmul.member.domain;
 
+import com.excmul.auth.AuthPrincipal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, Integer> {
+
+
     Optional<MemberEntity> findByEmail(Email email);
 
     boolean existsByEmail(Email email);
